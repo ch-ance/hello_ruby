4df623d9f599ad28a55dcf5cbc913c446f169915
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-11 00:35:13 -0500

    hello ruby!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-        render html: "hello, world!"
+        render html: '<h1>hello world</h1>'.html_safe
+    end
+
+    def goodbye
+        render html: '<h2>Goodbye, cold cold place</h2>'.html_safe
     end
 end
